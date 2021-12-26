7d8ce9d0e760c3f2dc2cf080010630984cf5c829
jubuyer <jahmed10@stuy.edu>
2021-12-26 01:03:56 -0500

    minor changes

--- Makefile
+++ Makefile
@@ -5,8 +5,16 @@ CC = g++
 CFLAGS = -std=c++11 #-Wall
 
 #targets
-main: main.o
+main: main.o task.o
 	$(CC) $(CFLAGS) -o $@ $^
 
 main.o: main.cpp
 	$(CC) $(CFLAGS) -c $^
+
+task.o: task.cpp
+	$(CC) $(CFLAGS) -c $^
+
+.PHONY: clean
+
+clean:
+	rm -f *o *out $(SDIR)/*out main
--- src/Task.h
+++ src/Task.h
@@ -1,18 +1,18 @@
 
-class Task () {
+class Task {
 private:
   std::string assignee; //who task is assigned to
   std::string info; //description of task
-  int deadline; //date to complete task by
+  std::string deadline; //date to complete task by
 public:
   Task();
   Task(std::string inputAssignee, std::string inputInfo, int inputDeadline);
   //getter methods
   std::string getAssignee();
   std::string getInfo();
-  int getDeadline;
+  std::string getDeadline;
   //modify object
   void changeAssignee(std::string newPerson);
   void updateInfo(std::string newInfo);
-  void updateDeadline(int newDeadline);
+  void updateDeadline(std::string newDeadline);
 };
